11b435c2a3daa833bbfc4483f9613730c5542ea5
shashank151 <sanutg@gmail.com>
2018-08-09 21:38:41 +0530

    "Completed module 2" -2

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,10 +8,10 @@ import { ProductService } from '../product.service';
 export class ProductDescriptionComponent implements OnInit {
 
   albumInfo;
-  constructor(private _productSerivce: ProductService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productSerivce.getAlbum(1).subscribe(response => this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
